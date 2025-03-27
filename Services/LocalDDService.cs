9142f83c3007d9ad462b67bb213aab97ccf5e280
HaRaRa221 <113466996+HaRaRa221@users.noreply.github.com>
2025-03-27 15:41:02 -0400

    Create Table Async

--- Services/LocalDDService.cs
+++ Services/LocalDDService.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SQLite;
+using Centralized_Lost_Found.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,19 @@ namespace Centralized_Lost_Found.Services
 {
     class LocalDDService
     {
+        private const string DB_NAME = "LostFound.db";
+
+        private readonly SQLiteAsyncConnection _connection;
+
+        public LocalDDService() 
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+
+            _connection.CreateTableAsync<User>();
+
+            _connection.CreateTableAsync<Item>();
+
+            
+        }
     }
 }
