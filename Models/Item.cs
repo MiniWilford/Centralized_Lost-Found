3cb0c0896fa790d815b9cdd6d9e524fcaa678be8
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2025-03-27 15:26:57 -0400

    Added Item Model

---
+++ Models/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Centralized_Lost_Found.Models
+{
+	// 3-27-2025: KD - Item Model for retrieving lost items and storing into the SQLite database
+	public class Item
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+		public string Location { get; set; }
+		public string Picture { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public bool IsFound { get; set; }
+		public string Uploader { get; set; }
+
+	}
+}
