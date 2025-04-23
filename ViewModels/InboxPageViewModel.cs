087cc803d267ed714b6dd1e0d1e02daa32fbe19b
HaRaRa221 <113466996+HaRaRa221@users.noreply.github.com>
2025-04-23 18:29:44 -0400

    InboxPage errors

--- ViewModels/InboxPageViewModel.cs
+++ ViewModels/InboxPageViewModel.cs
@@ -1,11 +1,53 @@
 ﻿using Centralized_Lost_Found.Services;
+using System.Collections.ObjectModel;
 using Centralized_Lost_Found.ViewModels;
 using Centralized_Lost_Found.Models;
+using System.Threading.Tasks;
+
+
+using System.Collections.ObjectModel;
 
 
 namespace Centralized_Lost_Found.ViewModels
 {
-    internal class InboxPageViewModel
+    public class InboxPageViewModel : BaseViewModel
     {
-    }
+        private readonly LocalDBService _dbService;
+        internal INavigation Navigation;
+
+        public ObservableCollection<InboxMessage> Messages { get; set; }
+        public Command RefreshCommand { get; }
+        public Command GoToUserProfilePageCommand { get; }
+
+        public InboxPageViewModel(LocalDBService dbService)
+        {
+            _dbService = dbService;
+            Messages = new ObservableCollection<InboxMessage>();
+            RefreshCommand = new Command(async () => await RefreshAsync());
+            GoToUserProfilePageCommand = new Command(async () => await GoToUserProfilePageAsync());
+
+        }
+
+        public async Task RefreshAsync()
+        {
+            // Logic to refresh inbox messages
+            var messages = await _dbService.GetInboxMessagesAsync();
+            Messages.Clear();
+            foreach (var message in messages)
+            {
+                Messages.Add(message);
+            }
+        }
+
+        public async Task GoToUserProfilePageAsync()
+        {
+            // Logic to navigate to user profile page
+        }
+
+        public async Task LoadUserHeaderAsync()
+        {
+            // Logic to load user header details
+        }
+
+    } 
 }
--- Views/InboxPage.xaml.cs
+++ Views/InboxPage.xaml.cs
@@ -6,9 +6,25 @@ namespace Centralized_Lost_Found.Views
 {
     public partial class InboxPage : ContentPage
     {
+        private InboxPageViewModel viewModel;
         public InboxPage()
         {
+            var dbService = App.Current.Handler.MauiContext.Services.GetService<LocalDBService>();
+            viewModel = new InboxPageViewModel(dbService);
+            BindingContext = viewModel;
             InitializeComponent();
+            viewModel.Navigation = this.Navigation;
+        }
+
+        protected override async void OnAppearing() 
+        {
+            base.OnAppearing();
+            if (viewModel != null) {
+
+                await viewModel.LoadUserHeaderAsync();
+                await viewModel.RefreshAsync();
+            }
+        
         }
     }
     
